7a62c3b4e4ee7626e281ba587ff2b171970607fd
Prince Codes <70610428+princecodes247@users.noreply.github.com>
2021-01-03 07:03:13 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
-//dbPassword = process.env.MONGODB_URI || 'mongodb://127.0.0.1/firsttime';
-dbPassword = "mongodb+srv://princecodes:princecodesatadmin@cluster0.cljyz.mongodb.net/firsttime?retryWrites=true&w=majority";
+dbPassword = process.env.MONGODB_URI || 'mongodb://127.0.0.1/cloudmark';
 
 module.exports = {
     mongoURI: dbPassword
